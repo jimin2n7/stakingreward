0b512ded2433016234ea60acf3654356bfb5f6a5
jimin2n7 <nguyenchiminhnt85@gmail.com>
2022-08-25 17:54:31 +0700

    update

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,11 +3,30 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     html{
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        
     }
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        &::-webkit-scrollbar
+        {
+            width: 5px;
+            background-color: #F5F5F5;
+        }
+
+        &::-webkit-scrollbar-track{
+            -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	        background-color: #F5F5F5;
+        }
+
+        &::-webkit-scrollbar-thumb
+        {
+            background-color: #ffcc00;
+            border: 2px solid #ffcc00;
+            border-radius: 5px;
+        }
     }
 
 `;
